ba0d101fd54420079740d763240a31e657fbf8b3
EJ-Manaloto <112523625+EJ-Manaloto@users.noreply.github.com>
2024-03-06 23:11:06 +0800

    Update Account.java

--- Account.java
+++ Account.java
@@ -21,5 +21,41 @@ public class Account {
     public double getBalance() {
         return balance;
     }
+    
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
 
-    public void setState(AccountState state
+    public void setState(AccountState state) {
+        this.state = state;
+    }
+
+    public void changeState(AccountState newState) {
+        this.state = newState;
+    }
+
+    public void deposit(double amount) {
+        state.deposit(this, amount); // Delegate to current state
+    }
+
+    public void withdraw(double amount) {
+        state.withdraw(this, amount); // Delegate to current state
+    }
+
+    public void suspend() {
+        state.suspend(this); // Delegate to current state
+    }
+
+    public void activate() {
+        state.activate(this); // Delegate to current state
+    }
+
+    public void close() {
+        state.close(this); // Delegate to current state
+    }
+
+    @Override
+    public String toString() {
+        return "Account Number: " + accountNumber + "\nBalance: $" + balance;
+    }
+}
